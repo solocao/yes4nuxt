b5ff810a4d806977960e7a2fef954b3b79b0925a
zemunkh <zemunkh@gmail.com>
2022-02-03 01:28:35 +0800

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Counter />
+</template>
+
+<script>
+import Counter from "~/components/Counter.vue";
+export default {
+    name: "IndexPage",
+    components: { Counter }
+}
+</script>
